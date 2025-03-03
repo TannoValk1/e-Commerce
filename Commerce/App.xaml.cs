ddd6ef30d13528cde6d3c9ddebf197bd63e67bb6
Opilane TTHK <opilane@tthk.ee>
2025-03-03 13:17:06 +0200

    Tegin projekti

---
+++ Commerce/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Commerce
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
